8b7eea1794fbf88a945ea9d6390ecc4309914668
Brad <Brad@LENOVO-PC>
2016-06-07 11:18:13 -0500

    Adding the initial Tagger implementation

---
+++ Cobol4VisualStudio.Extension/CobolTokenTagger.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace Cobol4VisualStudio.Extension {
+    public class CobolTokenTagger : ITagger<CobolTokenTag> {
+
+        private ITextBuffer Buffer;
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged
+        {
+            add { }
+            remove { }
+        }
+
+
+        public CobolTokenTagger(ITextBuffer buffer) {
+            this.Buffer = buffer;
+        }
+
+        public IEnumerable<ITagSpan<CobolTokenTag>> GetTags(NormalizedSnapshotSpanCollection spans) {
+            yield break;
+        }
+
+
+    }
+
+}
---
+++ Cobol4VisualStudio.Extension/CobolTokenTaggerProvider.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Cobol4VisualStudio.Extension {
+
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("Cobol")]
+    [TagType(typeof(CobolTokenTag))]
+    public class CobolTokenTaggerProvider : ITaggerProvider {
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag {
+            return new CobolTokenTagger(buffer) as ITagger<T>;
+        }
+    }
+
+}
