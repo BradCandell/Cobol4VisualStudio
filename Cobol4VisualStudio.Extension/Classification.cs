638685d0f7030b6e39a3acb1c4cdcc0d8bc2aafc
Brad Candell <bradcandell@live.com>
2016-06-28 13:15:53 -0500

    Removed the Classification code and moved to a single class without the Tagger Provider implementation.

---
+++ Cobol4VisualStudio.Extension/Classification.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Cobol4VisualStudio.Extension {
+
+    /// <summary>
+    /// Cobol Type Classification
+    /// </summary>
+    [ContentType("Cobol")]
+    internal sealed class CobolClassification {
+
+        /// <summary>
+        /// Content Type Definition
+        /// </summary>
+        [Export]
+        [Name("Cobol")]
+        [BaseDefinition("Code")]
+        internal static ContentTypeDefinition CobolContentType = null;
+
+        /// <summary>
+        /// File Extension to Content Type Definition (CBL)
+        /// </summary>
+        [Export]
+        [FileExtension(".cbl")]
+        [ContentType("Cobol")]
+        internal static FileExtensionToContentTypeDefinition CobolFileType = null;
+
+        /// <summary>
+        /// File Extension to Content Type Definition (CPY)
+        /// </summary>
+        [Export]
+        [FileExtension(".cpy")]
+        [ContentType("Cobol")]
+        internal static FileExtensionToContentTypeDefinition CopyBookFileType = null;
+
+    }
+}
